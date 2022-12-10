a4a4ba11f27af6b84b423e2fac3c70d9823254b4
Mindaugas Vilkas <46817295+johnsnow0@users.noreply.github.com>
2022-12-10 12:34:39 +0200

    code update

---
+++ components/NavOptions.js
@@ -0,0 +1,42 @@
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+import { Icon } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native'
+
+const data = [
+    {
+        id: '123',
+        title: 'Get a ride',
+        image: 'https://links.papareact.com/3pn',
+        screen: 'MapScreen'
+    },
+    {
+        id: '5435',
+        title: 'Order food',
+        image: 'https://links.papareact.com/28w',
+        screen: 'Eatscreen'
+    },
+]
+
+const NavOptions = () => {
+    const navigation = useNavigation();
+    return (
+        <FlatList
+            data={data}
+            keyExtractor={(item) => item.id}
+            horizontal
+            renderItem={({ item }) => (
+                <TouchableOpacity onPress={()=> navigation.navigate(item.screen)} className="p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40">
+                    <View>
+                        <Image style={{ width: 120, height: 120, resizeMode: 'contain' }}
+                            source={{ uri: item.image }} />
+                        <Text className="mt-2 text-lg font-semibold">{item.title}</Text>
+                        <Icon className="p-2 bg-black rounded-full w-10 mt-4" name='arrowright' color='white' type='antdesign' />
+                    </View>
+                </TouchableOpacity>
+            )}
+        />
+    )
+}
+
+export default NavOptions
---
+++ store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit'
+import navSlice from './slices/navSlice'
+
+export default configureStore({
+  reducer: {
+    nav: navSlice
+  }
+})
